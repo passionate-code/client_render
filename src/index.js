d13793a22e24cb113c2a8806deeefdf6a3e1f436
passionate-code <60539975+passionate-code@users.noreply.github.com>
2023-12-06 10:20:54 +0800

    #10

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-); /// 
+); 
 
 const root1 = createRoot(document.getElementById('root1'));
 root1.render(
@@ -19,7 +19,5 @@ root1.render(
   </React.StrictMode>
 );
 
-/// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
